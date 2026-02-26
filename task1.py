cbe0192568df505da8f456541e68024217b17633
bhatkartariq <bhatkartariq@eng.rizvi.edu.in>
2026-02-26 23:41:42 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,6 @@
+#Aim : Write a program to find if the entered number is even or odd
+number=float(input("ENTER A NUMBER: "))
+if number%2==0:
+    print("number is even")
+else: 
+    print("number is odd")
