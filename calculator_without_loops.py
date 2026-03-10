38c800a932ce9967b2d9066ab60ea1c7c65805a2
Tariq bhatkar <bhatkartariq@eng.rizvi.edu.in>
2026-03-10 23:33:34 +0530

    Add files via upload

---
+++ calculator_without_loops.py
@@ -0,0 +1,40 @@
+
+
+def add(a,b):
+    sum =a+b
+    return sum
+
+def minus(a,b):
+    sub =a-b
+    return sub
+
+def mul(a,b):
+    into =a*b
+    return into
+
+def divide(a,b):
+    
+    if b==0:
+        return "Not Defined"
+    else:
+        div=a/b
+    return  div
+
+print("--- Calculator ---\n")
+
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+opr=input("Enter your operation(=,-,*,/):")
+if opr=="+":
+    print(f"{num1} + {num2} = {add(num1,num2)}")
+elif opr=="-":
+    print(f"{num1} - {num2} = {minus(num1,num2)}")
+elif opr =="*":
+    print(f"{num1} * {num2} = {mul(num1,num2)}")
+elif opr=="/":
+    print(f"{num1} / {num2} = {divide(num1,num2)}")
+else:
+    print("error!!")
+
+hi=input("")
+
