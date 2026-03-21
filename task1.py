5cc7874dbfef3d2c0c1d7c98cb93b449e6e3f11d
Tariq bhatkar <bhatkartariq@eng.rizvi.edu.in>
2026-03-21 23:22:25 +0530

    Fix indentation and improve even/odd check logic

--- task1.py
+++ task1.py
@@ -1,6 +1,6 @@
 #Aim : Write a program to find if the entered number is even or odd
 number=float(input("ENTER A NUMBER: "))
-if number%2==0:
+if number%2==0:#condition for odd even 
     print("number is even")
 else: 
     print("number is odd")
